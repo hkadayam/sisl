ae65742353385c4cf32545848a0a46f10bdb0ef9
Hari Kadayam <hkadayam@ebay.com>
2025-05-16 20:05:09 -0700

    Build test binaries only if test enabled

--- conanfile.py
+++ conanfile.py
@@ -67,9 +67,9 @@ class SISLConan(ConanFile):
             self.options.rm_safe("prerelease")
             if self.options.coverage and self.options.sanitize:
                 raise ConanInvalidConfiguration("Sanitizer does not work with Code Coverage!")
-            if self.conf.get("tools.build:skip_test", default=False):
-                if self.options.coverage or self.options.sanitize:
-                    raise ConanInvalidConfiguration("Coverage/Sanitizer requires Testing!")
+            #if self.conf.get("tools.build:skip_test", default=False):
+            #    if self.options.coverage or self.options.sanitize:
+            #        raise ConanInvalidConfiguration("Coverage/Sanitizer requires Testing!")
 
     def build_requirements(self):
         self.test_requires("gtest/1.14.0")
